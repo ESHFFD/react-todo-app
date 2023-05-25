24b7af3056348795f9ad7c5a1ffb4915b91a4f1f
ESHFFD <ehsanshamsfallah@gmail.com>
2023-05-25 12:54:17 +0330

    added react router dom and implement it to our project

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@
 // import "./App.css";
 import Navbar from "./Navbar";
 import HomePage from "./HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <HomePage />
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
