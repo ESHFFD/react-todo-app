7ea1d3b89ce81fa9e6f9b5ca23f2a236f4b607e4
ESHFFD <ehsanshamsfallah@gmail.com>
2023-05-25 15:38:02 +0330

    impelment singel blog with some style

--- src/SingelBlog.js
+++ src/SingelBlog.js
@@ -1,10 +1,24 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const SingelBlog = () => {
   const { id } = useParams();
+  const {
+    data: blog,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs/" + id);
   return (
     <div className="singel-blog">
-      <h2>About blog - {id} </h2>
+      {isPending && <div>Is Loading ....</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 };
