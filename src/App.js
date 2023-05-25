98ae6da6f5dea05ed3e3720e88974a19effc5bcb
ESHFFD <ehsanshamsfallah@gmail.com>
2023-05-25 13:09:07 +0330

    activate our route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 import Navbar from "./Navbar";
 import HomePage from "./HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Create from "./Create";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/create" element={<Create />} />
           </Routes>
         </div>
       </div>
---
+++ src/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+  return (
+    <div className="create">
+      <h2>Here is create page</h2>
+    </div>
+  );
+};
+
+export default Create;
